dd3358e68a5cba96bcab76c287b2f0aac01702fb
Diksha Ninave <dikshaninave09@gmail.com>
2025-09-25 13:03:15 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.end("Hello World from CI/CD Pipeline!");
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
